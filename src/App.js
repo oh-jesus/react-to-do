fea9f517dd91d7b04aab1bef333a1c27c8fe9a7a
Jesus A Martinez <martinez18.rmhs@gmail.com>
2018-02-17 16:07:57 -0800

    update 7-17 3 PM

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor (props){
     super(props);
   this.state = {
-    todo : [
+    todos : [
          { description: 'Walk the cat', isCompleted: true },
          { description: 'Throw the dishes away', isCompleted: false },
          { description: 'Buy new dishes', isCompleted: false }
